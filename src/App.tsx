e9302ec886f270e821c9d1135652caf55c0ecb6f
Pauline Barnades <pauline.barnades@stenaline.com>
2022-11-19 17:51:16 +0100

    home-page components

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import './App.css';
 import AppRoutes from "./components/routes/AppRoutes";
 
+
 function App() {
   return (
       <AppRoutes />
-  );
+  )
 }
 
 export default App;
