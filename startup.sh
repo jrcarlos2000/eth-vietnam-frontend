47f0e36410f6d4c2cab1e85fa96f69b6de97529c
Konrad Gnat <konradmgnat@gmail.com>
2023-04-19 15:43:40 +0700

    update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+nvm use 16
 git reset --hard
 git pull
 yarn
 yarn build
 pm2 stop facets
 pm2 delete facets
-pm2 start npm --name=facets -- run start
+pm2 start yarn --name=facets -- run start
